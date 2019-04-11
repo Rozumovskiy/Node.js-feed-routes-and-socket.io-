65f9c2e5753f83a8f52b38b6ff239d062bb9009d
Bogdan <bogdanrozumovskij@gmail.com>
2019-04-11 14:51:33 +0300

    first commit

---
+++ Node/routes/feed.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const { check } = require("express-validator/check");
+
+const feedController = require("../controllers/feed");
+const isAuth = require("../middleware/is-auth");
+
+const router = express.Router();
+
+// GET /feed/posts
+router.get("/posts", isAuth, feedController.getPosts);
+
+// POST /feed/post
+router.post(
+  "/post",
+  isAuth,
+  [
+    check("title")
+      .trim()
+      .isLength({ min: 7 }),
+    check("content").isLength({ min: 5 })
+  ],
+  feedController.createPost
+);
+
+router.get("/post/:postId", isAuth, feedController.getPost);
+
+router.put(
+  "/post/:postId",
+  isAuth,
+  [
+    check("title")
+      .trim()
+      .isLength({ min: 7 }),
+    check("content").isLength({ min: 5 })
+  ],
+  feedController.updatePost
+);
+
+router.delete("/post/:postId", isAuth, feedController.deletePost);
+
+module.exports = router;
